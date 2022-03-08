6e2847fffbe16a1f246d1881f72d581e33df1efb
arpande97 <architpande888@gmail.com>
2022-03-08 11:45:26 +0530

    after initializing the spring project

---
+++ src/test/java/com/example/architrecipeproject/ArchitRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.architrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
